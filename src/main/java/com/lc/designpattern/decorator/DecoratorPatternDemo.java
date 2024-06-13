ea4fe39de9ecd0dcb3fa3c38abc647451c39db73
liucheng <1475494912@qq.com>
2024-06-13 18:41:05 +0800

    装饰器修改

--- src/main/java/com/lc/designpattern/decorator/DecoratorPatternDemo.java
+++ src/main/java/com/lc/designpattern/decorator/DecoratorPatternDemo.java
@@ -8,10 +8,12 @@ package com.lc.designpattern.decorator;
 public class DecoratorPatternDemo {
     public static void main(String[] args) {
         Shape circle = new Circle();
-        Shape redCircle = new RedShapeDecorator(new Circle());
+        Shape redShapeDecorator = new RedShapeDecorator(new Circle());
         System.out.println("Circle with normal border");
         circle.draw();
-        System.out.println("Circle of red border");
-        redCircle.draw();
+        System.out.println("Circle of other border");
+        redShapeDecorator.draw();
+        Shape yellowShapeDecorator = new YellowShapeDecorator(new Circle());
+        yellowShapeDecorator.draw();
     }
 }
